86ac6c5b8142c6074dc2a650b511ef48ab8c4b8e
Aaron Douglas <aarondoug212@gmail.com>
2022-12-06 15:36:05 -0500

    starting the create tournament page

--- vue/src/components/CreateTournament.vue
+++ vue/src/components/CreateTournament.vue
@@ -1,10 +1,29 @@
 <template>
-  <div></div>
+  <div>
+      <p>have a list of users here and select who you want in your tournament</p>
+      
+      <div v-for="user in users" v-bind:key="user.id" >
+        <p>{{user.username}}</p>
+      </div>
+      
+  </div>
 </template>
 
 <script>
-export default {
+import authService from '../services/AuthService'
 
+export default {
+data() {
+  return {
+    users: []
+  };
+},
+
+created() {
+  authService.getAllUsers().then((response) => {
+    this.users = response.data;
+  })
+}
 }
 </script>
 
---
+++ vue/src/views/CreateTournament.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <create-tournament/>
+  </div>
+</template>
+
+<script>
+import CreateTournament from '../components/CreateTournament.vue'
+export default {
+  components: { CreateTournament },
+
+}
+</script>
+
+<style>
+
+</style>
