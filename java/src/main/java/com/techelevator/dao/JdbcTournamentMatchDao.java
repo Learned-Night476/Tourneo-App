75fcde4d31c9e7f7145cc62878c3f5159bf69b9f
Ethan Hughey <ethan.hughey1@gmail.com>
2022-12-09 15:18:54 -0500

    get tournament by round and id

--- java/src/main/java/com/techelevator/dao/JdbcTournamentMatchDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcTournamentMatchDao.java
@@ -1,8 +1,13 @@
 package com.techelevator.dao;
 
+import com.techelevator.model.Player;
 import com.techelevator.model.TournamentMatch;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class JdbcTournamentMatchDao implements TournamentMatchDao {
@@ -22,14 +27,37 @@ public class JdbcTournamentMatchDao implements TournamentMatchDao {
 
 
         }
+
+    @Override
+    public List<TournamentMatch> getTournamentMatchsByTournamentIdAndRound(int tournamentId, int round) {
+        List<TournamentMatch> matches = new ArrayList<>();
+        String sql = "Select match_id, round, winner, tournament_id, player_id, away_player_id from tournament_match WHERE round = ? and tournament_id = ?";
+
+
+        SqlRowSet rs = jdbcTemplate.queryForRowSet(sql, tournamentId, round);
+
+        while (rs.next()) {
+            TournamentMatch match = mapRowToTournament(rs);
+            matches.add(match);
+        }
+
+        return matches;
+
     }
-//    public boolean createTournamentMatch(TournamentMatch tournamentMatch) {
-//
-//        String sql = "insert into tournament_match (home, away, round, tournament_id) values (?, ?, ?, ?); ";
-//
-//        Integer newUserId = jdbcTemplate.queryForObject(sql, Integer.class, tournamentMatch.getHomeId(), tournamentMatch.getAwayId(), tournamentMatch.getRound(), tournamentMatch.getTournamentId());
-//
-//        return true;
-//
-//    }
-//}
+
+    private TournamentMatch mapRowToTournament(SqlRowSet rs) {
+        TournamentMatch tournamentMatch = new TournamentMatch();
+
+        tournamentMatch.setMatchId(rs.getInt("match_id"));
+        tournamentMatch.setAwayPlayerId(rs.getInt("away_player_id"));
+        tournamentMatch.setPlayerId(rs.getInt("player_id"));
+        tournamentMatch.setRound(rs.getInt("round"));
+        tournamentMatch.setWinner(rs.getInt("winner"));
+        tournamentMatch.setTournamentId(rs.getInt("tournament_id"));
+
+        return tournamentMatch;
+    }
+
+
+}
+
--- java/src/main/java/com/techelevator/dao/TournamentMatchDao.java
+++ java/src/main/java/com/techelevator/dao/TournamentMatchDao.java
@@ -2,8 +2,12 @@ package com.techelevator.dao;
 
 import com.techelevator.model.TournamentMatch;
 
+import java.util.List;
+
 public interface TournamentMatchDao {
 
     void createTournamentMatch(TournamentMatch tournamentMatch);
+
+    List<TournamentMatch> getTournamentMatchsByTournamentIdAndRound(int tournamentId, int round);
 
 }
