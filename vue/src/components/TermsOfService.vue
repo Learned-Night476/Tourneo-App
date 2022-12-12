0abf517f6f69d25b2314bd580130b7410adfcdf2
Learned-Night476 <112592987+Learned-Night476@users.noreply.github.com>
2022-12-12 14:40:55 -0500

    Terms update

--- vue/src/components/TermsOfService.vue
+++ vue/src/components/TermsOfService.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="terms-of-service">
       <h1>Terms of Service </h1>
+      <p>Do you even lift bro?Are you interested in a ping pong tournament to the death? <br>
+      To compete in Tourneo you must be willing to have fun, if you're a kill joy, just go <br>
+      to some other lame tournament site. We won't name names, but you know who we're talking about.<br>
+      Tourneo is all about challenging yourself and others. The Arena is sacred place where dreams <br>
+      live and champions are made. If you think you have what it takes, step into the Arena.</p>
+  
   </div>
 </template>
 
@@ -11,5 +17,10 @@ name: "terms-of-service"
 </script>
 
 <style>
+h1, p{
+  text-align: center;
+  font-size: 15px;
+}
+
 
 </style>
